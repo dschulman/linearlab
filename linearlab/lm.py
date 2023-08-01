085b1573626ec02157163a3d3f8624ea7983db56
Daniel Schulman <danieljschulman@gmail.com>
2023-08-01 08:20:42 -0400

    bugfixes and lm example

--- linearlab/lm.py
+++ linearlab/lm.py
@@ -11,17 +11,17 @@ class LM:
         self.X = X
 
     @staticmethod
-    def from_formula(data: pd.DataFrame, form: str) -> LM:
+    def from_formula(data: pd.DataFrame, form: str) -> "LM":
         y, X = formulaic.model_matrix(form, data)
         return LM(y.squeeze(), X)
 
-    def fit(self) -> LMResults:
+    def fit(self) -> "LMResults":
         n = self.y.size
         y = self.y.to_numpy(np.float_)
         X = self.X.to_numpy(np.float_)
-        beta = npl.lstsq(X.T @ X, X.T @ y)
+        beta = npl.lstsq(X.T @ X, X.T @ y, rcond=None)[0]
         y_hat = X @ beta
-        rss = np.sum((y - z)**2)
+        rss = np.sum((y - y_hat)**2)
         sigma2 = rss / n
         loglik = -0.5 * n * (LOG_2PI + np.log(sigma2) + 1)
         return LMResults(
