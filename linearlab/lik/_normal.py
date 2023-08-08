6722c239ba15e65245233d7322f262deaf49396a
Daniel Schulman <danieljschulman@gmail.com>
2023-08-08 07:12:01 -0400

    normal lik: bugfix, specialize for log scale

--- linearlab/lik/_normal.py
+++ linearlab/lik/_normal.py
@@ -2,7 +2,7 @@ import numpy as np
 import numpy.typing as npt
 
 from linearlab.lik.base import Likelihood
-from linearlab.link import Link, identity, log
+from linearlab.link import Link, identity, LogLink, log
 from linearlab.util import LOG_2PI
 
 class Normal(Likelihood):
@@ -15,7 +15,7 @@ class Normal(Likelihood):
 
     def __call__(
         self, 
-        y: npt.NDArray[np.bool_],
+        y: npt.NDArray[np.float64],
         eta: npt.NDArray[np.float64]
     ) -> tuple[float, npt.NDArray[np.float64], npt.NDArray[np.float64]]:
         mu, dmu = self.mu_link.inv(eta[0])
@@ -25,11 +25,40 @@ class Normal(Likelihood):
         gmu = dmu * z / sigma
         gsigma = dsigma * (z**2 - 1) / sigma
         g = np.stack([gmu, gsigma])
-        hmu = 1 / sigma**2
-        hsigma = 2 / sigma**2
+        hmu = dmu**2 / sigma**2
+        hsigma = 2 * dsigma**2 / sigma**2
         hmu_sigma = np.zeros(y.shape[0])
         h = np.stack([[hmu, hmu_sigma], [hmu_sigma, hsigma]])
         return f, g, h
 
+class NormalLogScale(Likelihood):
+    def __init__(self, mu_link: Link) -> None:
+        self.mu_link = mu_link
+
+    def params(self) -> list[str]:
+        return ["mu", "sigma"]
+
+    def __call__(
+        self, 
+        y: npt.NDArray[np.float64],
+        eta: npt.NDArray[np.float64]
+    ) -> tuple[float, npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+        mu, dmu = self.mu_link.inv(eta[0])
+        log_sigma = eta[1]
+        sigma = np.exp(log_sigma)
+        z = (y - mu) / sigma
+        f = -0.5 * np.sum(LOG_2PI + 2*log_sigma + z**2)
+        gmu = dmu * z / sigma
+        gsigma = (z**2 - 1) / sigma
+        g = np.stack([gmu, gsigma])
+        hmu = 1 / sigma**2
+        hsigma = np.full_like(hmu, 2.0)
+        hmu_sigma = np.zeros(y.shape[0])
+        h = np.stack([[hmu, hmu_sigma], [hmu_sigma, hsigma]])
+        return f, g, h  
+
 def normal(mu_link: Link = identity, sigma_link: Link = log) -> Likelihood:
-    return Normal(mu_link, sigma_link)
+    if isinstance(sigma_link, LogLink):
+        return NormalLogScale(mu_link)
+    else:
+        return Normal(mu_link, sigma_link)
